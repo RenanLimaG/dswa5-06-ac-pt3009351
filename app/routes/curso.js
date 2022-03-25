b97864912bdfb7636dc82356e3c6cd0b0541dd10
RenanLimaG <renanlimag10@gmail.com>
2022-03-24 21:01:01 -0300

    Update curso.js

--- app/routes/curso.js
+++ app/routes/curso.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
     var controller = app.controllers.curso;
-    app.route('/cursos').get(controller.listaCurso);
+    app.route('/cursos').get(controller.listaCursos);
 	app.route('/cursos/:id').get(controller.obtemCurso)
 	.delete(controller.removeCurso);
 };
